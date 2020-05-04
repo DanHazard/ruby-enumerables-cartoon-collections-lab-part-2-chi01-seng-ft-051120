2aee3692c1cd3e0df4f2dc38a6537e6d20feb92d
Dan Love <daniel.love.jr@gmail.com>
2020-05-04 05:10:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,6 +34,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.find do |str|
-    valid_calls.include?()
+    valid_calls.include?(str)
+  end
   
 end
